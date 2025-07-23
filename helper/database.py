5731d8e6c3534c4387d44b5bb60d9b6233f61da3
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2025-07-24 01:09:58 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -57,7 +57,7 @@ class Database:
             uploadlimit=Config.FREE_UPLOAD_LIMIT,
             daily=0,
             metadata_mode=False,
-            metadata_code="--change-title @Rkn_Botz\n--change-video-title @Rkn_Botz\n--change-audio-title @Rkn_Botz\n--change-subtitle-title @Rkn_Botz\n--change-author @Rkn_Botz",
+            metadata_code="--change-title @AYU_Bots\n--change-video-title @AYU_Bots\n--change-audio-title @AYU_Bots\n--change-subtitle-title @AYU_Bots\n--change-author @AYU_Bots",
             expiry_time=None,
             has_free_trial=False,
             ban_status=dict(
